b5fe034ddad193d6b6ce6a085bef879973b57ed0
Harut Vartanyan <haroutaraara@gmail.com>
2023-03-01 16:41:03 +0400

    js

--- js.js
+++ js.js
@@ -1,56 +1,34 @@
-const username = prompt('please input your name')
-const chatMessages = document.getElementById('chat-messages');
-const sendButton = document.getElementById('send-button');
-const messageInput = document.getElementById('message-input');
-const errorMessage = document.getElementById('error-message')
+const images = document.querySelectorAll('.container img')
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const text = document.querySelectorAll('.container p')
 
-const sendMessage = () => {
+let current = 0
 
-  const message = messageInput.value.trim();
+next.addEventListener('click', () => {
 
-    if (!message) {
-      errorMessage.innerHTML = 'input cannot be blank'
-      errorMessage.style.display = 'block'
+  images[current].classList.remove('active')
+  text[current].classList.remove('active')
+  current++
 
-      setTimeout(() => {
-        errorMessage.style.display = 'none'
-      }, 1000);
-      return
-    } else {
-      errorMessage.style.display = 'none'
-    }
-
-    const usernameEl = document.createElement('div');
-
-    chatMessages.appendChild(usernameEl)
-    usernameEl.classList.add('username');
-
-
-      const today = new Date()
-    
-    const day = `${today.getDate() < 10 ? "0" + today.getDate() : today.getDate()}`
-    const month = `${(today.getMonth() + 1) < 10 ? "0" + (today.getMonth() + 1) : (today.getMonth() + 1)}`
-    const year = `${today.getFullYear()}`
-    const hour = `${today.getHours() < 10 ? "0" + today.getHours() : today.getHours()}`
-    const minute = `${today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes()}`
-    const second = `${today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds()}`
-
-    usernameEl.textContent = `${username} - ${day}/${month}/${year} - ${hour}:${minute}:${second}`
-    
-  const messageEl = document.createElement('div');
-  
-  messageEl.classList.add('chat-message');
-  messageEl.textContent = message;
-  chatMessages.appendChild(messageEl);
-  messageInput.value = '';
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-};
-
-
-sendButton.addEventListener('click', sendMessage);
-
-messageInput.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-    sendMessage();
+  if (current === images.length) {
+    current = 0
   }
-});
+
+  images[current].classList.add('active')
+  text[current].classList.add('active')
+})
+
+prev.addEventListener('click', () => {
+
+  images[current].classList.remove('active')
+  text[current].classList.remove('active')
+  current--
+
+  if (current < 0) {
+    current = (images.length - 1)
+  }
+
+  images[current].classList.add('active')
+  text[current].classList.add('active')
+})
